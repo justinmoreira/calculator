da46b7b58bfb278f63cabb9ba8460273d6ca84fa
Justin Moreira <justinmoreira23@gmail.com>
2023-08-30 17:53:38 -0400

    Change some functionality, calculate continuous operations

--- function.js
+++ function.js
@@ -1,3 +1,68 @@
+//let temp = '';
+let a = '';
+let b = '';
+let operator = '';
+let resetNext = false;
+//let previousResult;
+
+const numbers = document.querySelectorAll('.num');
+const operations = document.querySelectorAll('.oper');
+const display = document.querySelector('.results');
+const equal = document.querySelector('.equal');
+    
+equal.addEventListener('click', equals);
+
+numbers.forEach((num) => num.addEventListener('click', () => displayNum(num.id)));
+
+operations.forEach((oper) => oper.addEventListener('click', () => displayOper(oper.id)));
+
+/*for(let i = 0; i < num.length; i++){
+    num[i].addEventListener('click', function(){
+        temp += this.id;
+        display.textContent = temp;
+    });
+};
+for(let i = 0; i < oper.length; i++){
+    oper[i].addEventListener('click', function(){
+        a = temp;
+        operator = this.id;
+        display.textContent = operator;
+            
+        for(let j = 0; j < num.length; j++){
+            num[j].addEventListener('click', function(){
+                b += this.id;
+                display.textContent = b;
+            });
+        };
+    });
+};*/
+function displayNum(num) {
+    if(resetNext){display.textContent = '';}
+    resetNext = false;
+    display.textContent += num;
+}
+function displayOper(oper) {
+    a = display.textContent;
+    operator = oper;
+    display.textContent = oper;
+    resetNext = true;
+}
+
+function equals(){
+    b = display.textContent;
+
+    a = +a;
+    b = +b;
+
+    display.textContent = operate(a, b, operator);
+    /*previousResult = operate(a, b, operator)
+    display.textContent = previousResult;
+    temp = previousResult;
+    a = '';
+    b = '';
+    operator = '';*/
+}
+
 function operate(a, b, oper){
     if(oper == '+') { return a+b; }
     if(oper == '-') { return a-b; } 
@@ -5,61 +70,6 @@ function operate(a, b, oper){
     if(oper == '/') { return a/b; }
 }
 
-
-
-function letsPlay(){
-    let temp = '';
-    let a = '';
-    let b = '';
-    let operator = '';
-
-    const num = document.querySelectorAll('.num');
-    const oper = document.querySelectorAll('.oper');
-    const output = document.querySelector('.results');
-    const display = document.createElement('div');
-    output.append(display);
-    const equal = document.querySelector('.equal');
-    
-    for(let i = 0; i < num.length; i++){
-        num[i].addEventListener('click', function(){
-            temp += num[i].id;
-            display.textContent = temp;
-        });
-    };
-    for(let i = 0; i < oper.length; i++){
-        oper[i].addEventListener('click', function(){
-            a = temp;
-            operator = oper[i].id;
-            display.textContent = operator;
-            for(let j = 0; j < num.length; j++){
-                num[j].addEventListener('click', function(){
-                    b += num[j].id;
-                    display.textContent = b;
-                });
-            };
-        });
-    };
-    equal.addEventListener('click', function(){
-        display.textContent = operate(a, b, operator);
-    });
-    
-    
-    /*for(let j = 0; j < oper.length; j++){
-        while(!oper[j].addEventListener('click', function() {
-            operator = oper[j].id;
-            display.textContent = operator;
-            but[i].addEventListener('click', function() {
-                    b += but[i].id;
-                    display.textContent = b;
-            });
-        })){
-            for(let i = 0; i < but.length; i++){
-                a += but[i].id;
-                display.textContent = a;
-            };
-        }
-    }*/
-    
+function resetDisplay(){
+    display.textContent = '';
 }
-
-letsPlay();
